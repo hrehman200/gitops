f775db2240d8b67e51273bebe7c4722c6a7e2d8a
Haris ur Rehman <hrehman200@gmail.com>
2021-09-20 01:25:09 +0500

    All requirements completed .. just need to set script.py flow

--- gitops.py
+++ gitops.py
@@ -1,26 +1,64 @@
 import yaml
 import git
-from git import RemoteProgress
 import os
+import zipfile
+from shutil import copyfile
+import smtplib
+from email.message import EmailMessage
+import xml.etree.ElementTree as ET
+import logging
+import subprocess
+
+log = object
 
 def loadConfig(configPath):
     with open('config.yml') as c:
         config = yaml.load(c, yaml.SafeLoader)
         return config
 
+def setLogging(log_file_path):
+    logging.basicConfig(format='[%(asctime)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO,
+    handlers=[
+        logging.FileHandler(log_file_path),
+        logging.StreamHandler()
+    ])
+    log = logging.getlog(__name__)
+    log.info('Setting logging')
+
 def repoCloned(path):
-    return os.path.isdir(path)
+    cloned = os.path.isdir(path)
+    log.info(f"""Repo {path }exists : {cloned}""")
+    return cloned
 
 def cloneRepo(url, branch, folder):
+    log.info(f"""Cloning repo {url} {branch} to dir {folder} """)
     return git.Repo.clone_from(url, folder, branch=branch)
+
+def pullRepo(repo_path):
+    log.info(f"""Pulling repo {getRepoName(repo_path)} """)
+    repo = git.Repo(repo_path)
+    o = repo.remotes.origin
+    o.pull()
+
+def getRepoHash(repo_path):
+    repo = git.Repo(repo_path)
+    sha = repo.head.object.hexsha
+    return sha
 
 def getRepoObj(folder):
     return git.Repo(folder)
 
-def remoteAhead(repo_obj, branch):
+def getRepoName(repo_path):
+    repo = git.Repo(repo_path)
+    return repo.remotes.origin.url.split('.git')[0].split('/')[-1]
+
+def remoteAhead(repo_path, branch):
+    repo_obj = git.Repo(repo_path)
     commits_diff = repo_obj.git.rev_list('--left-right', '--count', f'{branch}...{branch}@{{u}}')
     num_ahead, num_behind = commits_diff.split('\t')
-    return int(num_ahead) > 0
+    ahead = int(num_ahead) > 0
+    log.info(f"""Repo {getRepoName(repo_path)} is ahead? : {ahead}""")
+    return ahead
 
 def getValueFromVersionFile(version_file_path, key):
     with open(version_file_path) as f:
@@ -35,6 +73,7 @@ def updateValueInVerionFile(version_file_path, key, oldValue, newValue):
         lines = f.readlines()
         for index, line in enumerate(lines):
             if(key in line):
+                log.info(f"""Updating {key} to : {newValue}""")
                 lines[index] = line.replace(oldValue, newValue)
     
     with open(version_file_path, 'w') as f:
@@ -48,10 +87,91 @@ def incrementVersion(old_version):
     return ".".join(parts)
 
 def commitAndPushRepo(repo_path, message):
-    repoObj = git.Repo(repo_path)
-    repoObj.index.commit(message)
-    origin = repoObj.remote(name='origin')
+    repo_obj = git.Repo(repo_path)
+    repo_obj.index.commit(message)
+    origin = repo_obj.remote(name='origin')
     origin.push()
-    
+    log.info(f"""Pushed {getRepoName(repo_path)}""")
 
+def tagRepo(repo_path, message):
+    repo_obj = git.Repo(repo_path)
+    repo_obj.create_tag(message)
+    origin = repo_obj.remote(name='origin')
+    origin.push(message)
+    log.info(f"""Tagged {getRepoName(repo_path)} with {message}""")
+
+def renameArtefactsFolder(folder_path, new_name):
+    parts = folder_path.split('/')
+    parts[len(parts) - 1] = new_name
+    new_path = "/".join(parts)
+    os.rename(folder_path, new_path)
+    log.info(f"""Renamed artefacts folder from {folder_path} to {new_path}""")
+    return new_path
+    
+def zipdir(src, dst):
+    os.chdir(os.path.dirname(src))
+    zf = zipfile.ZipFile("%s.zip" % (dst), "w", zipfile.ZIP_DEFLATED)
+    abs_src = os.path.abspath(src)
+    for dirname, subdirs, files in os.walk(src):
+        for filename in files:
+            absname = os.path.abspath(os.path.join(dirname, filename))
+            arcname = absname[len(abs_src) + 1:]
+            zf.write(absname, arcname)
+    zf.close()
+    log.info(f"""Zipped artefacts directory {abs_src+".zip"}""")
+    return abs_src+".zip"
+
+def copyFileToPath(src, dst):
+    copyfile(src, dst)
+
+def sendEmail(to, subject, message):
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = subject
+    msg['From'] = 'sender@gmail.com'
+    msg['To'] = to
+
+    with smtplib.SMTP("mail.siemens.de") as server:
+        #server.login(user, password)
+        server.send_message(msg)
+        log.info(f"""Sent email with subject {subject} to {to}""")
+
+def updateValuesInManifest(xml_file, projects):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    
+    for dict in projects:
+        #dict = project.attrib
+
+        target_node = root.find(".//*[@name='"+dict['name']+"']")
+
+        if('_version' in dict['name']):
+            log.ino('Setting ' + dict['name'] +' revision to : ' + dict['revision'])
+            target_node.set('revision', dict['revision'])
+        else:
+            keys = dict.keys()
+ 
+            target_node = root.find(".//*[@name='"+dict['name']+"']")
+            if(target_node is None):
+                log.info('Appending project ' + str(dict))
+                target_node = root.makeelement('project', dict)
+                root.append(target_node)
+
+            for attr in keys:
+                if(attr in ['revision', 'upstream']):
+                    log.info('Setting ' + dict['name'] + ' ' + attr +
+                        ' to : ' + dict[attr])
+                    target_node.set(attr, dict[attr])
+
+                    log.info('Setting ' + dict['name'] +
+                        ' upstream to : ' + dict['revision'])
+                    target_node.set('upstream', dict['revision'])
+
+    tree.write(xml_file, encoding="UTF-8", xml_declaration=True)
+    
+def runBuildScript(script_path):
+    log.info(f"""Running build script {script_path}""")
+    output = subprocess.Popen(script_path, shell=True, stdout=subprocess.PIPE).stdout.read()
+    log.info(f"""Build script complete: {output}""")
+    return output
 
--- script.py
+++ script.py
@@ -1,40 +1,91 @@
+import os
 from sys import version
 import gitops
+from datetime import datetime
 
-config = gitops.loadConfig('./config.yml')
+cfg = gitops.loadConfig('./cfg.yml')
 
-version_repo = config['VERSION_REPO_BRANCH']
-version_repo_path = config['PATH_REPO_SAVE_FOLDER']+config['VERSION_REPO_NAME']
-version_repo_url = config['GIT_REMOTE_BASE_URL']+config['VERSION_REPO_NAME']
+gitops.setLogging(cfg['PATH_SCRIPT_LOG'])
+
+version_repo = cfg['VERSION_REPO_BRANCH']
+version_repo_path = cfg['PATH_REPO_SAVE_FOLDER']+cfg['VERSION_REPO_NAME']
+version_repo_url = cfg['GIT_REMOTE_BASE_URL']+cfg['VERSION_REPO_NAME']
 version_file_path = version_repo_path+'/version_build.h'
 version_xml_path = version_repo_path+'/manifest.xml'
 
-repositories = config['REPOSITORIES']
+repositories = cfg['REPOSITORIES']
+projects_xml = []
 
+# clone version repo
 if gitops.repoCloned(version_repo_path) is False:
-    gitops.cloneRepo(version_repo_url, config['VERSION_REPO_BRANCH'], version_repo_path)
+    gitops.cloneRepo(version_repo_url, cfg['VERSION_REPO_BRANCH'], version_repo_path)
+
+# pull version repo
+if gitops.remoteAhead(version_repo_path, cfg['VERSION_REPO_BRANCH']):
+    gitops.pullRepo(version_repo_path)
+    projects_xml.append({
+        'name':cfg['VERSION_REPO_NAME'], 
+        'revision':gitops.getRepoHash()   
+    })
 
 build_necessary = False
-for repo in repositories:
-    repo_path = config['PATH_REPO_SAVE_FOLDER'] + repo['name']
-    repo_url = config['GIT_REMOTE_BASE_URL'] + repo['name']
-    if gitops.repoCloned(repo_path) is False:
-        repo_obj = gitops.cloneRepo(repo_url, repo['branch'], repo_path)
-    else:
-        repo_obj = gitops.getRepoObj(repo_path)
 
-    if gitops.remoteAhead(repo_obj, repo['branch']):
+# clone or pull and decide whether build necessary
+for repo in repositories:
+    repo_path = cfg['PATH_REPO_SAVE_FOLDER'] + repo['name']
+    repo_url = cfg['GIT_REMOTE_BASE_URL'] + repo['name']
+    if gitops.repoCloned(repo_path) is False:
+        gitops.cloneRepo(repo_url, repo['branch'], repo_path)
+    elif gitops.remoteAhead(repo_path, repo['branch']):
         build_necessary = True
 
 version_num = gitops.getValueFromVersionFile(version_file_path, 'VERSION')
 version_date = gitops.getValueFromVersionFile(version_file_path, 'DATE')
 
-if(True): #build_necessary
-    # gitops.updateValueInVerionFile(version_file_path, 'VERSION', version_num, 'NEWVERSION')
-    # gitops.updateValueInVerionFile(version_file_path, 'DATE', version_num, 'NEWDATE')
+new_version_num = gitops.incrementVersion(version_num)
 
-    new_version_num = gitops.incrementVersion(version_num)
-    print(new_version_num)
+if(True): #build_necessary
+
+    for repo in repositories:
+        repo_path = cfg['PATH_REPO_SAVE_FOLDER'] + repo['name']
+        repo_url = cfg['GIT_REMOTE_BASE_URL'] + repo['name']
+        gitops.cloneRepo(repo_url, repo['branch'], repo_path)
+        projects_xml.append({
+            'name':repo['name'], 
+            'revision':gitops.getRepoHash(),
+            'upstream':repo['branch']
+        })
+
+    build_output = gitops.runBuildScript(cfg['PATH_BUILD_SCRIPT'])
+    # need to decide build_successful
+    build_successful = 'success' in build_output
+
+    gitops.updateValuesInManifest(version_xml_path, projects_xml)
+    
+    if(build_successful):
+        gitops.tagRepo(version_repo_path, cfg['TAG_PREFIX']+new_version_num)
 
     gitops.updateValueInVerionFile(version_file_path, 'VERSION', version_num, new_version_num)
-    gitops.commitAndPushRepo(version_repo_path, 'Success')
+    gitops.updateValueInVerionFile(version_file_path, 'DATE', version_date, datetime.now().strftime("%Y/%m/%d_%H:%M:%S"))
+    gitops.commitAndPushRepo(version_repo_path, cfg['COMMIT_MESSAGE_PASS'])
+
+    artefact_build_dir = cfg['PATH_BUILD_ARTEFACTS_SOURCE']
+    artefact_dir_new_name = cfg['ARTEFACTS_PREFIX']+'_'+new_version_num
+    artefact_build_dir = gitops.renameArtefactsFolder(artefact_build_dir, artefact_dir_new_name)
+
+    zipped_file_path = gitops.zipdir(artefact_build_dir, artefact_dir_new_name)
+    zip_filename = os.path.basename(zipped_file_path)
+    zip_file_on_network = cfg['PATH_BUILD_ARTEFACTS_DESTINATION']+zip_filename
+
+    gitops.copyFileToPath(zipped_file_path, zip_file_on_network)
+
+    email_message = f"""
+        Build Result {zip_file_on_network}
+        Build Manifest {version_xml_path}
+        Build Log {cfg['PATH_BUILD_LOG']}
+    """
+
+    gitops.sendEmail(cfg['SEND_MAIL_DISTRIBUTION_LIST'], 'Build Results ' + new_version_num, email_message)
+
+else:
+    gitops.sendEmail(cfg['SEND_MAIL_DISTRIBUTION_LIST'], 'Build Results ' + new_version_num, 'No build necessary')
